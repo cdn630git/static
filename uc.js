5ab00e103d46b88454d8b8fd66f1549813726b9f
cdn630git <46342267+cdn630git@users.noreply.github.com>
2019-01-03 17:15:03 +0800

    Create uc.js

---
+++ uc.js
@@ -0,0 +1,32 @@
+var index3=""; //棣栭〉澶撮儴
+var index4=""; //鍐呴〉澶撮儴
+var index5=""; //搴曢儴
+
+if(IsBan3()){
+if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iphone/i)){
+	document.writeln("<A href=\"https://m.53cu.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1YRIfacfrK1Rjy1Xd5jaemFXa2.gif\" width=\"960\" height=\"137\"><\/A>");
+	document.writeln("<A href=\"https://m.jsvip08.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1_RwfajDuK1RjSszd5jXGLpXa0.gif\" width=\"960\" height=\"70\"><\/A>");
+	document.writeln("<A href=\"https://m.jsvip08.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1fm6.ae6sK1RjSsrb5jXbDXXa1.gif\" width=\"960\" height=\"70\"><\/A>");
+	document.writeln("<A href=\"https://m.zy155.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://i.loli.net/2018/08/19/5b79268caf516.jpg\" width=\"960\" height=\"55\"><\/A>");
+	document.writeln("<A href=\"https://m.zy155.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://i.loli.net/2018/08/19/5b7926c7a65bf.jpg\" width=\"960\" height=\"55\"><\/A>");
+	document.writeln("<A href=\"https://m.zy155.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://i.loli.net/2018/08/19/5b792709d83d6.jpg\" width=\"960\" height=\"55\"><\/A>");	
+    document.writeln("<A href=\"https://m.46ts.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1QYZdac_vK1Rjy0Fo5jXIxVXaN.gif\" width=\"960\" height=\"70\"><\/A>");
+    document.writeln("<A href=\"https://m.46ts.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1B1Y.aovrK1RjSspc5jbzSXXaZ.gif\" width=\"960\" height=\"70\"><\/A>");
+    document.writeln("<A href=\"https://m.jsvip08.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1nR3facrrK1RjSspa5jcREXXak.gif\" width=\"960\" height=\"137\"><\/A>");
+    document.writeln("<A href=\"https://m.8hha.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1O1cdainrK1RjSszi5jXptpXat.gif\" width=\"960\" height=\"70\"><\/A>");
+    document.writeln("<A href=\"https://m.8hha.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1O1cdainrK1RjSszi5jXptpXat.gif\" width=\"960\" height=\"70\"><\/A>");	
+	}else{
+	document.writeln("<A href=\"https://www.53cu.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1YRIfacfrK1Rjy1Xd5jaemFXa2.gif\" width=\"960\" height=\"137\"><\/A>");
+	document.writeln("<A href=\"https://www.jsvip08.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1_RwfajDuK1RjSszd5jXGLpXa0.gif\" width=\"960\" height=\"70\"><\/A>");
+	document.writeln("<A href=\"https://www.jsvip08.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1fm6.ae6sK1RjSsrb5jXbDXXa1.gif\" width=\"960\" height=\"70\"><\/A>");
+	document.writeln("<A href=\"https://www.zy155.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://i.loli.net/2018/08/19/5b79268caf516.jpg\" width=\"960\" height=\"55\"><\/A>");
+	document.writeln("<A href=\"https://www.zy155.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://i.loli.net/2018/08/19/5b7926c7a65bf.jpg\" width=\"960\" height=\"55\"><\/A>");
+	document.writeln("<A href=\"https://www.zy155.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://i.loli.net/2018/08/19/5b792709d83d6.jpg\" width=\"960\" height=\"55\"><\/A>");
+	document.writeln("<A href=\"https://www.46ts.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1QYZdac_vK1Rjy0Fo5jXIxVXaN.gif\" width=\"960\" height=\"70\"><\/A>");
+    document.writeln("<A href=\"https://www.46ts.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1B1Y.aovrK1RjSspc5jbzSXXaZ.gif\" width=\"960\" height=\"70\"><\/A>");
+    document.writeln("<A href=\"https://www.jsvip08.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1nR3facrrK1RjSspa5jcREXXak.gif\" width=\"960\" height=\"137\"><\/A>");
+    document.writeln("<A href=\"https://www.8hha.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1O1cdainrK1RjSszi5jXptpXat.gif\" width=\"960\" height=\"70\"><\/A>");
+    document.writeln("<A href=\"https://www.8hha.com/main.html?m=reg\" target=\"_blank\"><img border=\"0\" src=\"https://ae01.alicdn.com/kf/HTB1O1cdainrK1RjSszi5jXptpXat.gif\" width=\"960\" height=\"70\"><\/A>");	
+	}
+}
+
